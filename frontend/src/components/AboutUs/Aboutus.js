81a61c039b3a8ad36a7ceb59daaa88c2fbb3dee1
Abhay Kumar Ray <abhayray2002@gmail.com>
2022-10-31 17:57:48 +0530

    added responsiveness in about us

--- frontend/src/components/AboutUs/Aboutus.js
+++ frontend/src/components/AboutUs/Aboutus.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import { Image } from '@chakra-ui/image';
 import { useMediaQuery } from '@chakra-ui/media-query';
-import { Stack, HStack, Flex, Box, Text, Heading } from '@chakra-ui/layout';
+import {
+  Stack,
+  HStack,
+  Flex,
+  Box,
+  Text,
+  Heading,
+  Container,
+} from '@chakra-ui/layout';
 import './About.css';
-import a1 from '../../assests/images/avataricons/1co-f.svg'
-import a2 from '../../assests/images/avataricons/2co-f.svg'
-import a3 from '../../assests/images/avataricons/3co-f.svg'
-import a4 from '../../assests/images/avataricons/4co-f.svg'
-import a5 from '../../assests/images/avataricons/5co-f.svg'
-import aboutUsImg from '../../assests/images/aboutus.jpg'
+import a1 from '../../assests/images/avataricons/1co-f.svg';
+import a2 from '../../assests/images/avataricons/2co-f.svg';
+import a3 from '../../assests/images/avataricons/3co-f.svg';
+import a4 from '../../assests/images/avataricons/4co-f.svg';
+import a5 from '../../assests/images/avataricons/5co-f.svg';
+import aboutUsImg from '../../assests/images/aboutus.jpg';
 import { Link } from 'react-router-dom';
-
 
 function Aboutus() {
   const [isNotSmallerScreen] = useMediaQuery('(min-width:600px)');
@@ -19,16 +26,13 @@ function Aboutus() {
     <Stack>
       <HStack>
         <Flex
-          direction={isNotSmallerScreen ? 'row' : 'column'}
+          direction={['column','row']}
           spacing="200px"
-          p={isNotSmallerScreen ? '32' : '0'}
+          p={[10, 20]}
           alignSelf="flex-start"
         >
           <div className="imageabout">
-            <Image
-              paddingTop={30}
-              src={aboutUsImg}
-            />{' '}
+            <Image paddingTop={30} src={aboutUsImg} />{' '}
           </div>
           <div>
             <Box mt={isNotSmallerScreen ? '0' : 16} align="flex-start">
@@ -44,7 +48,8 @@ function Aboutus() {
               <Text fontSize="17px" fontWeight="">
                 Skillery was a dream which was envisioned 4 months back to make
                 programming education easy and accessible for Indian students.
-                We provide premium content at a very reasonable subscription cost.{' '}
+                We provide premium content at a very reasonable subscription
+                cost.{' '}
               </Text>
               <Link to="/subscribe">
                 <Heading color="yellow.400" fontSize="xl">
@@ -55,105 +60,38 @@ function Aboutus() {
           </div>
         </Flex>
       </HStack>
-      <HStack>
-        <div>
-          <Flex
-            rounded="l"
-            direction="column"
-            mt={4}
-            ml={isNotSmallerScreen ? 4 : 0}
-            bg="gray.100"
-            h="33vh"
-            w="33vh"
-            justify="flex-end"
-            _hover={{ bg: 'teal.400' }}
-            marginLeft={20}
-          >
-            <Image src= {a5} />
-            <Text color="black" p="4" fontSize="l" fontWeight="semibold">
-              Abhay Ray
-            </Text>
-          </Flex>
-        </div>
-        <div>
-          <Flex
-            rounded="l"
-            direction="column"
-            mt={4}
-            ml={isNotSmallerScreen ? 4 : 0}
-            bg="gray.100"
-            h="33vh"
-            w="33vh"
-            justify="flex-end"
-            _hover={{ bg: 'teal.400' }}
-            marginLeft={10}
-          >
-            <Image src= {a1} />
-            <Text color="black" p="4" fontSize="l" fontWeight="semibold">
-              Shubham Chandwani
-            </Text>
-          </Flex>
-        </div>
-        <div>
-          <Flex
-            rounded="l"
-            direction="column"
-            mt={4}
-            ml={isNotSmallerScreen ? 4 : 0}
-            bg="gray.100"
-            h="33vh"
-            w="33vh"
-            justify="flex-end"
-            _hover={{ bg: 'teal.400' }}
-            marginLeft={10}
-          >
-            <Image src={a2} />
-            <Text color="black" p="4" fontSize="l" fontWeight="semibold">
-              Tanmay Pawar
-            </Text>
-          </Flex>
-        </div>
-        <div>
-          <Flex
-            rounded="l"
-            direction="column"
-            mt={4}
-            ml={isNotSmallerScreen ? 4 : 0}
-            bg="gray.100"
-            h="33vh"
-            w="33vh"
-            justify="flex-end"
-            _hover={{ bg: 'teal.400' }}
-            marginLeft={10}
-          >
-            <Image src={a4} />
-            <Text color="black" p="4" fontSize="l" fontWeight="semibold">
-              Ashutosh Rawat
-            </Text>
-          </Flex>
-        </div>
-        <div>
-          <Flex
-            rounded="l"
-            direction="column"
-            mt={4}
-            ml={isNotSmallerScreen ? 4 : 0}
-            bg="gray.100"
-            h="33vh"
-            w="33vh"
-            justify="flex-end"
-            _hover={{ bg: 'teal.400' }}
-            marginLeft={10}
-          >
-            <Image src={a3} />
-            <Text color="black" p="4" fontSize="l" fontWeight="semibold">
-              Tanmay Umredkar
-            </Text>
-          </Flex>
-        </div>
-      </HStack>
-
-      <br />
+      <Stack direction={['column', 'row']} alignContent="center">
+        <Container>
+          <Image src={a5} />
+          <Text _hover={{ bg: 'yellow.400' }} color="black" p="4" fontSize="l" fontWeight="semibold" textAlign={'center'}>
+            Abhay Kumar Ray
+          </Text>
+        </Container>
+        <Container>
+          <Image src={a4} />
+          <Text _hover={{ bg: 'yellow.400' }} color="black" p="4" fontSize="l" fontWeight="semibold" textAlign={'center'}>
+            Tanmay Umredkar
+          </Text>
+        </Container>
+        <Container>
+          <Image src={a3} />
+          <Text _hover={{ bg: 'yellow.400' }} color="black" p="4" fontSize="l" fontWeight="semibold" textAlign={'center'}>
+            Tanmay Pawar
+          </Text>
+        </Container>
+        <Container>
+          <Image src={a2} />
+          <Text _hover={{ bg: 'yellow.400' }} color="black" p="4" fontSize="l" fontWeight="semibold" textAlign={'center'}>
+            Shubham Chandwani
+          </Text>
+        </Container>
+        <Container>
+          <Image src={a1} />
+          <Text _hover={{ bg: 'yellow.400' }} color="black" p="4" fontSize="l" fontWeight="semibold" textAlign={'center'}>
+            Ashutosh Rawat
+          </Text>
+        </Container>
+      </Stack>
     </Stack>
   );
 }
