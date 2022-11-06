69d0c7dcad0a24bad2d2fb846ada043eec6df423
Abhay Kumar Ray <abhayray2002@gmail.com>
2022-11-06 23:18:42 +0530

    added new course api

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,13 @@
 import app from "./app.js";
+import { connectDB } from "./config/database.js";
+
+connectDB()
+  .then((connection) => {
+    console.log(`MongoDB connected with ${connection.host}`);
+  })
+  .catch((err) => {
+    console.log(`Mongo error : ${err.message}`);
+  });
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port: ${process.env.PORT}`);
