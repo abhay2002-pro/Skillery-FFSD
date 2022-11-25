8a65466e7b315d1329868d6fb32958f6b9d6f3a7
Abhay Kumar Ray <abhayray2002@gmail.com>
2022-11-25 09:36:55 +0530

    create course upload feature added

--- backend/controllers/courseController.js
+++ backend/controllers/courseController.js
@@ -1,8 +1,11 @@
 import { catchAsyncError } from "../middlewares/catchAsyncError.js";
 import { Course } from "../models/Course.js";
+import getDataUri from "../utils/dataUri.js";
 import ErrorHandler from "../utils/errorHandler.js";
+import cloudinary from "cloudinary";
+
 export const getAllCourses = catchAsyncError(async (req, res, next) => {
-  const courses = await Course.find().select("-lectures"); // We don't want lectures data 
+  const courses = await Course.find().select("-lectures"); // We don't want lectures data
   res.status(200).json({
     success: true,
     courses,
@@ -16,7 +19,11 @@ export const createCourse = catchAsyncError(async (req, res, next) => {
     return next(new ErrorHandler("Please add all fields", 400));
   }
 
-  // const file = req.file;
+  const file = req.file;
+  console.log(file)
+  const fileUri = getDataUri(file)
+
+  const mycloud = await cloudinary.v2.uploader.upload(fileUri.content);
 
   await Course.create({
     title,
@@ -24,12 +31,58 @@ export const createCourse = catchAsyncError(async (req, res, next) => {
     category,
     createdBy,
     poster: {
-      public_id: "temp",
-      url: "temp",
+      public_id: mycloud.public_id,
+      url: mycloud.secure_url,
     },
   });
   res.status(201).json({
     success: true,
     message: "Course Created Successfully. You can add lectures now.",
+  });
+});
+
+export const addLecture = catchAsyncError(async (req, res, next) => {
+
+  const {title, description} = req.body;
+  const {id} = req.params;
+
+  // const file = req.file
+
+  const course = await Course.findById(id);
+
+  if (!course) return next(new ErrorHandler("Course not found", 404));
+
+  //upload file here
+
+  course.lectures.push({
+    title,
+    description,
+    video: {
+      public_id: "url",
+      url: "url",
+    },
+  });
+
+  course.numOfVideos = course.lectures.length;
+
+  await course.save();
+
+  res.status(201).json({
+    success: true,
+    message: "Lecture add in course",
+  });
+});
+export const getCourseLectures = catchAsyncError(async (req, res, next) => {
+  const course = await Course.findById(req.params.id);
+
+  if (!course) return next(new ErrorHandler("Course not found", 404));
+
+  course.views += 1;
+
+  await course.save();
+
+  res.status(201).json({
+    success: true,
+    lectures: course.lectures,
   });
 });
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,20 @@
 import app from "./app.js";
 import { connectDB } from "./config/database.js";
+import cloudinary from "cloudinary";
 
 connectDB()
-  .then((connection) => {
-    console.log(`MongoDB connected with ${connection.host}`);
-  })
-  .catch((err) => {
-    console.log(`Mongo error : ${err.message}`);
-  });
+.then((connection) => {
+  console.log(`MongoDB connected with ${connection.host}`);
+})
+.catch((err) => {
+  console.log(`Mongo error : ${err.message}`);
+});
+
+cloudinary.v2.config({
+  cloud_name: process.env.CLOUDINARY_CLIENT_NAME,
+  api_key: process.env.CLOUDINARY_CLIENT_API,
+  api_secret: process.env.CLOUDINARY_CLIENT_SECRET
+})
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port: ${process.env.PORT}`);
