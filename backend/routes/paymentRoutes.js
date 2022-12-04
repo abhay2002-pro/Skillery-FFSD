d1bb5bb6891afc0f46fca26cea791f8c10c62ade
Abhay Kumar Ray <abhayray2002@gmail.com>
2022-12-04 08:31:31 +0530

    cancel subscription api completed

--- backend/routes/paymentRoutes.js
+++ backend/routes/paymentRoutes.js
@@ -1,9 +1,19 @@
 import express from "express";
-import { buySubscription } from "../controllers/paymentController.js";
+import { buySubscription, cancelSubscription, getRazorPayKey, paymentVerification } from "../controllers/paymentController.js";
 import { isAuthenticated } from "../middlewares/auth.js";
 
 const router = express.Router();
 
 //Buy subscription
 router.route("/subscribe").get(isAuthenticated, buySubscription)
+
+//payment verification and save reference in db
+router.route("/paymentverification").post(isAuthenticated, paymentVerification)
+
+// Get Razorpay key
+router.route("/razorpaykey").get(getRazorPayKey)
+
+// Cancel subscription
+router.route("/subscribe/cancel").delete(isAutenticated, cancelSubscription)
+
 export default router;
