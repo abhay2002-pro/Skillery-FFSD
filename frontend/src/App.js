8f751ff73ce227d3921c550ff501fd0f9added55
Abhay Kumar Ray <abhayray2002@gmail.com>
2022-12-05 13:46:30 +0530

    refractoring frontend code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,58 +1,229 @@
-import React, { useState } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Contact from './components/ContactUs/Contact';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import ForgetPassword from './components/Auth/ForgetPassword';
+import Login from './components/Auth/Login';
+import Register from './components/Auth/Register';
+import ResetPassword from './components/Auth/ResetPassword';
+import Contact from './components/Contact/Contact';
 import Courses from './components/Courses/Courses';
-import Home from './components/Home';
+import Home from './components/Home/Home';
 import Footer from './components/Layout/Footer/Footer';
-import Aboutus from './components/AboutUs/Aboutus';
 import Header from './components/Layout/Header/Header';
-import NotFound from './components/Layout/NotFound/NotFound';
+import Request from './components/Request/Request';
+import About from './components/About/About';
+import Subscribe from './components/Payments/Subscribe';
 import PaymentFail from './components/Payments/PaymentFail';
 import PaymentSuccess from './components/Payments/PaymentSuccess';
-import Subscribe from './components/Payments/Subscribe';
-import Request from './components/Request/Request';
-import CreateCourse from './components/Admin/CreateCourse';
-import Login from './components/Login/Login';
-import Register from './components/Register/Register';
+import NotFound from './components/Layout/NotFound/NotFound';
+import CoursePage from './components/CoursePage/CoursePage';
+import Profile from './components/Profile/Profile';
+import ChangePassword from './components/Profile/ChangePassword';
+import UpdateProfile from './components/Profile/UpdateProfile';
+import Dashboard from './components/Admin/Dashboard/Dashboard';
+import CreateCourse from './components/Admin/CreateCourse/CreateCourse';
+import AdminCourses from './components/Admin/AdminCourses/AdminCourses';
+import Users from './components/Admin/Users/Users';
+import { useDispatch, useSelector } from 'react-redux';
+import toast, { Toaster } from 'react-hot-toast';
+import { loadUser } from './redux/actions/user';
+import { ProtectedRoute } from 'protected-route-react';
+import Loader from './components/Layout/Loader/Loader';
 
 function App() {
-  const [courses, setCourses] = useState([
-    {
-      title: 'Sample1',
-      description: 'Sample1',
-      imageSrc:
-        'https://images.unsplash.com/photo-1495592822108-9e6261896da8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
-      id: 'sample1',
-      creator: 'sample1 creator',
-    },
-  ]);
-  const coursesHandler = newCourse => {
-    setCourses(previousList => {
-      return [...previousList, newCourse];
-    });
-  };
-  return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/courses" element={<Courses courses={courses} />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/request" element={<Request />} />
-        <Route path="/about" element={<Aboutus />} />
-        <Route path="/subscribe" element={<Subscribe />} />
-        <Route path="/*" element={<NotFound />} />
-        <Route path="/paymentsuccess" element={<PaymentSuccess />} />
-        <Route path="/paymentfail" element={<PaymentFail />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/Register" element={<Register />} />
+  window.addEventListener('contextmenu', e => {
+    e.preventDefault();
+  });
 
-        {/* Admin Routes */}
-        <Route path="/admin/createcourse" element={<CreateCourse coursesHandler={coursesHandler} />} />
-      </Routes>
-      <Footer />
-    </BrowserRouter>
+  const { isAuthenticated, user, message, error, loading } = useSelector(
+    state => state.user
+  );
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (error) {
+      toast.error(error);
+      dispatch({ type: 'clearError' });
+    }
+
+    if (message) {
+      toast.success(message);
+      dispatch({ type: 'clearMessage' });
+    }
+  }, [dispatch, error, message]);
+
+  useEffect(() => {
+    dispatch(loadUser());
+  }, [dispatch]);
+
+  return (
+    <Router>
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <Header isAuthenticated={isAuthenticated} user={user} />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/courses" element={<Courses />} />
+
+            <Route
+              path="/course/:id"
+              element={
+                <ProtectedRoute isAuthenticated={isAuthenticated}>
+                  <CoursePage user={user} />
+                </ProtectedRoute>
+              }
+            />
+
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/request" element={<Request />} />
+            <Route path="/about" element={<About />} />
+
+            <Route
+              path="/profile"
+              element={
+                <ProtectedRoute isAuthenticated={isAuthenticated}>
+                  <Profile user={user} />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/changepassword"
+              element={
+                <ProtectedRoute isAuthenticated={isAuthenticated}>
+                  <ChangePassword />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/updateprofile"
+              element={
+                <ProtectedRoute isAuthenticated={isAuthenticated}>
+                  <UpdateProfile user={user} />
+                </ProtectedRoute>
+              }
+            />
+
+            <Route
+              path="/login"
+              element={
+                <ProtectedRoute
+                  isAuthenticated={!isAuthenticated}
+                  redirect="/profile"
+                >
+                  <Login />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/register"
+              element={
+                <ProtectedRoute
+                  isAuthenticated={!isAuthenticated}
+                  redirect="/profile"
+                >
+                  <Register />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/forgetpassword"
+              element={
+                <ProtectedRoute
+                  isAuthenticated={!isAuthenticated}
+                  redirect="/profile"
+                >
+                  <ForgetPassword />
+                </ProtectedRoute>
+              }
+            />
+
+            <Route
+              path="/resetpassword/:token"
+              element={
+                <ProtectedRoute
+                  isAuthenticated={!isAuthenticated}
+                  redirect="/profile"
+                >
+                  <ResetPassword />
+                </ProtectedRoute>
+              }
+            />
+
+            <Route
+              path="/subscribe"
+              element={
+                <ProtectedRoute isAuthenticated={isAuthenticated}>
+                  <Subscribe user={user} />
+                </ProtectedRoute>
+              }
+            />
+
+            <Route path="*" element={<NotFound />} />
+
+            <Route path="/paymentsuccess" element={<PaymentSuccess />} />
+
+            <Route path="/paymentfail" element={<PaymentFail />} />
+
+            {/* Admin Routes */}
+            <Route
+              path="/admin/dashboard"
+              element={
+                <ProtectedRoute
+                  adminRoute={true}
+                  isAuthenticated={isAuthenticated}
+                  isAdmin={user && user.role === 'admin'}
+                >
+                  <Dashboard />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/admin/createcourse"
+              element={
+                <ProtectedRoute
+                  adminRoute={true}
+                  isAuthenticated={isAuthenticated}
+                  isAdmin={user && user.role === 'admin'}
+                >
+                  <CreateCourse />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/admin/courses"
+              element={
+                <ProtectedRoute
+                  adminRoute={true}
+                  isAuthenticated={isAuthenticated}
+                  isAdmin={user && user.role === 'admin'}
+                >
+                  <AdminCourses />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/admin/users"
+              element={
+                <ProtectedRoute
+                  adminRoute={true}
+                  isAuthenticated={isAuthenticated}
+                  isAdmin={user && user.role === 'admin'}
+                >
+                  <Users />
+                </ProtectedRoute>
+              }
+            />
+          </Routes>
+
+          <Footer />
+          <Toaster />
+        </>
+      )}
+    </Router>
   );
 }
 
 export default App;
+
+// Admin admincourses
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,10 @@
+import { ColorModeScript } from '@chakra-ui/react';
 import React, { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import App from './App';
 import { ChakraProvider, theme } from '@chakra-ui/react';
 import { Provider as ReduxProvider } from 'react-redux';
-import store from "./components/redux/store"
+import store from './redux/store';
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
@@ -12,6 +13,7 @@ root.render(
   <StrictMode>
     <ReduxProvider store={store}>
       <ChakraProvider theme={theme}>
+        <ColorModeScript />
         <App />
       </ChakraProvider>
     </ReduxProvider>
