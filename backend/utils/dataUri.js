57c1bfb9b967800236a341a76ecd61d3bf2d8e4c
Abhay Kumar Ray <abhay.ray@theinternetfolks.com>
2023-03-26 17:25:39 +0530

    small changes

--- backend/utils/dataUri.js
+++ backend/utils/dataUri.js
@@ -1,5 +1,6 @@
 import DataUriParser from "datauri/parser.js";
 import path from "path";
+
 const getDataUri = (file) => {
   const parser = new DataUriParser();
   const extName = path.extname(file.originalname).toString();
